2eb2338ab88a526a7870747c8daa6be08e26132b
Zach Ross-Clyne <zach.ross-clyne@hotmail.co.uk>
2016-01-30 22:55:03 +0000

    I make a ui thing please eat my face

--- ShamanShowdown/UserInterface.h
+++ ShamanShowdown/UserInterface.h
@@ -7,7 +7,16 @@ public:
 	UserInterface();
 	~UserInterface();
 
+	int health1;
+	int health2;
+	int health3;
+	int health4;
+
+	int randcount;
+
 	void update(GameState * state, float deltaTime, Controls* controls, GameObject * object);
 	void render(Renderer * renderer);
+
+
 };
 
