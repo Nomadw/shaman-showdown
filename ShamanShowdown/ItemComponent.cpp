bff41cf0f77a9fba89ae58a0b1fa7ae2a37b8b2c
Zachhoward-smith <zach.howard-smith@hotmail.com>
2016-01-30 13:40:38 +0000

    Minor

---
+++ ShamanShowdown/ItemComponent.cpp
@@ -0,0 +1,29 @@
+#include "ItemComponent.h"
+#include "Renderer.h"
+#include "GameState.h"
+#include "Controls.h"
+#include "GameObject.h"
+#include "TransformComponent.h"
+
+ItemComponent::ItemComponent(int textureID)
+{
+
+}
+
+
+ItemComponent::~ItemComponent()
+{
+}
+
+
+void ItemComponent::update(GameState* state, float deltaTime, Controls* controls, GameObject* object)
+{
+	x = object->transform->Translation().getX() - 0.5f;
+	y = object->transform->Translation().getY() - 0.5f;
+	size = 1.0f * 64.0f;
+}
+
+void ItemComponent::render(Renderer * renderer)
+{
+	renderer->draw(texture, x * 64, y * 64, size);
+}
