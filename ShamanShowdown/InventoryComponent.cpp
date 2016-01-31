0362d0ed691c12f3db2d90487c50e8b3358858ac
Zach Ross-Clyne <zach.ross-clyne@hotmail.co.uk>
2016-01-31 12:13:22 +0000

    Added Inventory

--- ShamanShowdown/InventoryComponent.cpp
+++ ShamanShowdown/InventoryComponent.cpp
@@ -1,8 +1,14 @@
 #include "InventoryComponent.h"
-
+#include "TeamMemberComponent.h"
+#include "GameObject.h"
+#include "GameState.h"
+#include "Controls.h"
+#include "TransformComponent.h"
+#include "MagicProjectileComponent.h"
 
 InventoryComponent::InventoryComponent()
 {
+	inventory = queue<Items>();
 }
 
 
@@ -10,12 +16,51 @@ InventoryComponent::~InventoryComponent()
 {
 }
 
-void InventoryComponent::update()
+void InventoryComponent::update(GameState * state, float deltaTime, Controls* controls, GameObject * object)
 {
-
+	char key;
+	int team = ((TeamMemberComponent *)object->getComponent<TeamMemberComponent>())->team;
+	if (team == 0) {
+		key = 'Q';
+	}
+	else {
+		key = 'O';
+	}
+	
+	if (controls->isKeyReleased(key) && (state->getTeam(1 - team).GetWarrior() != NULL || state->getTeam(1 - team).GetShaman() != NULL))
+	{
+		useItem(state, team, object);
+	}
 }
 
-void InventoryComponent::render()
+void InventoryComponent::useItem(GameState* state, int team, GameObject* object)
 {
+	if (inventory.size() > 0)
+	{
+		Items nextItem = inventory.front();
 
+		switch (nextItem)
+		{
+		case missle:
+			GameObject * missleObject = new GameObject();
+			missleObject->transform->Translation() = object->transform->Translation();
+			if (state->getTeam(1 - team).GetWarrior() != NULL)
+			{
+				missleObject->attachComponent(new MagicProjectileComponent(0, 25, state->getTeam(1 - team).GetWarrior()->transform, missleObject->transform, team));
+			}
+			else if (state->getTeam(1 - team).GetShaman() != NULL)
+			{
+				missleObject->attachComponent(new MagicProjectileComponent(0, 25, state->getTeam(1 - team).GetShaman()->transform, missleObject->transform, team));
+			}
+			state->addGameObject(missleObject);
+			break;
+		}
+
+		inventory.pop();
+	}
+}
+
+void InventoryComponent::giveItem(Items item)
+{
+	inventory.push(item);
 }
--- ShamanShowdown/InventoryComponent.h
+++ ShamanShowdown/InventoryComponent.h
@@ -1,14 +1,18 @@
 #pragma once
 //STL Includes
-
+#include <stack>
+#include <queue>
 //Custom Includes
 #include "GameComponent.h"
+#include "ItemComponent.h"
 
 using namespace std;
 class InventoryComponent : public GameComponent
 {
 #pragma region Variables
 public:
+
+	queue<Items> inventory;
 private:
 #pragma endregion
 
@@ -26,8 +30,11 @@ private:
 #pragma region Functions
 public:
 	//Overriders for the abstract base GameComponent
-	void update();
-	void render();
+	void render(Renderer * renderer) {}
+	void update(GameState * state, float deltaTime, Controls* controls, GameObject * object);
+
+	void useItem(GameState* state, int team, GameObject* object);
+	void giveItem(Items item);
 private:
 #pragma endregion
 
